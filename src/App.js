8699dd05aa03cec949f51705b9a352621964b591
rvanukur <rvanukur@calliduscloud.com>
2018-11-27 21:20:30 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,159 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import FormField from './components/ui/FormFields/FormField';
 
 class App extends Component {
+  state={
+    createForm:{
+      first_name:{
+        type:'text',
+        label: 'no',
+        value:'',
+        attributes:{
+          placeholder: 'First Name',
+          className:'input',
+          name:'first_name',
+          value:''
+        },
+        validation:{
+          min:3,
+          max:10,
+          required:"required",
+        }
+      },
+
+      address:{
+        type:'textarea',
+        label: 'no',
+        attributes:{
+          placeholder: 'Address',
+          className:'input',
+          name:'address',
+          value:''
+        },
+        validation:{
+          min:3,
+          max:10,
+          required:"required",
+        }
+      },
+
+
+      education:{
+        type:'select',
+        label: 'no',
+        attributes:{
+          placeholder: 'Education',
+          className:'input',
+          name:'education',
+          value:'Inter'
+        },
+        options:[
+          {label: "10th", value:"10th"},
+          {label: "Inter", value:"Inter"},
+          {label: "Degree", value:"Degree"}
+        ],
+        validation:{
+          required:"required",
+        }
+      },
+
+
+      gender:{
+        type:'radio',
+        label: 'no',
+        attributes:{
+          placeholder: 'Gender',
+          className:'input',
+          name:'gender',
+          value:'male'
+        },
+        options:[
+          {label: "Male", value:"male"},
+          {label: "Female", value:"female"}
+        ],
+        validation:{
+          required:"required",
+        }
+      },
+
+      languages:{
+        type:'checkbox',
+        label: 'no',
+        attributes:{
+          placeholder: 'Languages',
+          className:'input',
+          name:'languages',
+          value:''
+        },
+        options:[
+          {label: "Telugu", value:"Telugu", selected:true},
+          {label: "English", value:"English", selected:false},
+          {label: "Hindi", value:"Hindi", selected:false}
+        ],
+
+        validation:{
+          required:"required",
+        }
+      },
+
+
+
+
+    }
+  }
+
+
+
+  inputChangedHandler = (event,key)=>{
+    let oldFormData= {...this.state.createForm};
+    // console.log(event.target.checked, event.target.value , key);
+    if(oldFormData[key].type ==='checkbox'){
+      oldFormData[key].options.forEach(single=>{
+        if(event.target.value === single.value){
+          single.selected=!single.selected;
+        }
+      });
+      let finalSelectedOptions=[];
+      oldFormData[key].options.forEach(single=>{
+        if(single.selected){
+              finalSelectedOptions.push(single.value);
+          }
+      });
+      oldFormData[key].attributes.value = finalSelectedOptions;
+
+    }else{
+      oldFormData[key].attributes.value = event.target.value;
+    }
+
+    this.setState({createForm:oldFormData},()=>{
+      console.log(this.state.createForm);
+    })
+  }
+
+  buildForm= ()=>{
+    let finalFields=[];
+    for (let field in this.state.createForm){
+      finalFields.push({key:field, fieldValues: this.state.createForm[field]});
+      // console.log(field);
+    }
+    let finalForm=finalFields.map(item=>{
+      return(<FormField fieldValues={item.fieldValues} inputChangedHandler={(event)=>this.inputChangedHandler(event,item.key)} key={item.key} />);
+    });
+
+    return finalForm;
+
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <form>
+        
+          {this.buildForm()}
+       
+        <input type="submit" value="Submit" />
+        </form>
       </div>
     );
   }
---
+++ src/components/ui/FormFields/FormField.js
@@ -0,0 +1,54 @@
+import React from 'react';
+
+const FormField = (props) => {
+    let returnField=null;
+
+    switch(props.fieldValues.type){
+
+        case ('text'):
+        returnField = <input type={props.fieldValues.type} {...props.fieldValues.attributes} {...props.fieldValues.validation} onChange={(event)=>props.inputChangedHandler(event)} />
+        break;
+
+        case ('textarea'):
+        returnField = <textarea type={props.fieldValues.type} {...props.fieldValues.attributes} {...props.fieldValues.validation} onChange={(event)=>props.inputChangedHandler(event)} />
+        break;
+
+        case ('select'):
+        returnField = (<select type={props.fieldValues.type} {...props.fieldValues.attributes} {...props.fieldValues.validation} onChange={(event)=>props.inputChangedHandler(event)}>
+        <option value=''>Select {props.fieldValues.attributes.placeholder}</option>
+        {props.fieldValues.options.map(single=>{
+            return <option value={single.value} key={single.value}>{single.label}</option>;
+        })}
+        </select>)
+        break;
+
+
+        case ('radio'):
+        returnField = (<div>
+            {props.fieldValues.options.map(single=>{
+            return <label key={single.value}><input type={props.fieldValues.type} {...props.fieldValues.attributes} {...props.fieldValues.validation} onChange={(event)=>props.inputChangedHandler(event)} value={single.value} checked={single.value===props.fieldValues.attributes.value} />{single.label}</label>;
+        })}
+        </div>)
+        break;
+
+
+        case ('checkbox'):
+        returnField = (<div>
+    
+
+        {props.fieldValues.options.map(single=>{
+            return <label key={single.value}><input type={props.fieldValues.type} {...props.fieldValues.attributes} {...props.fieldValues.validation} onChange={(event)=>props.inputChangedHandler(event)} value={single.value} checked={single.selected} />{single.label}</label>;
+        })}
+
+        </div>)
+        break;
+
+
+        default:
+        returnField = <input type={props.fieldValues.type} {...props.fieldValues.attributes} {...props.fieldValues.validation} onChange={(event)=>props.inputChangedHandler(event)} />
+
+    }
+    return returnField;
+}
+
+export default FormField;
